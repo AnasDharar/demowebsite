ee593ba9a8b2f2ad0e43b79ef16448d4049c7f9c
Anas Dharar <dhararanas94@gmail.com>
2025-01-03 22:13:45 +0530

    commiting the files

---
+++ script.js
@@ -0,0 +1,12 @@
+ //for Seeing Nav Bar
+ function openfunc(){
+    document.getElementById("menu").style.width="300px";
+}
+//For closing nav Bar
+function closefunc(){
+   document.getElementById("menu").style.width="0px";
+}
+
+function submit(){
+  alert('Registered Successfully!');
+}
